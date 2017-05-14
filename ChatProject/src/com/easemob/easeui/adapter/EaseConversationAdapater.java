5fe1029cc7f1d4e29599fb4d23ca5226db184db8
sayid0927 <13113639501@163.com>
2017-05-15 00:20:21 +0800

    Create README.md

--- ChatProject/src/com/easemob/easeui/adapter/EaseConversationAdapater.java
+++ ChatProject/src/com/easemob/easeui/adapter/EaseConversationAdapater.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.text.TextUtils;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -50,10 +49,8 @@ import com.easemob.util.NetUtils;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.ListIterator;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.ListIterator;
 
 
 /**
